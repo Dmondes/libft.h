a1065af80467c7ef90412f75a4f27885b1fe5ca4
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-14 15:43:27 +0800

    Update ft_str_is_alpha.c

--- functions/ft_str_is_alpha.c
+++ functions/ft_str_is_alpha.c
@@ -1,6 +1,6 @@
 int ft_str_is_alpha(int c)
 {
-    if((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+    if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
         return (1);
     return (0);
 }
