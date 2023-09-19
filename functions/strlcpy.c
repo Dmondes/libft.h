b2dfa51150a0d7df32d5898417777ff168fa7655
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-19 16:06:34 +0800

    Update strlcpy.c

--- functions/strlcpy.c
+++ functions/strlcpy.c
@@ -1,1 +1,17 @@
+#include "libft.h"
 
+size_t  ft_strlcpy(char *dest, const char *src, size_t size) 
+{
+    size_t  i;
+    i = 0;
+    while (src[i])
+    {
+        if (i < (size - 1))
+        {
+            dest[i] = src[i];
+        }
+        i ++;
+    }
+    dest[i] = '\0';
+    return (i);
+}
