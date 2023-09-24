3aab4d69089f2d273942fb39b56e94cc0b1975cf
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-24 12:56:21 +0800

    Update ft_memset.c

--- functions/ft_memset.c
+++ functions/ft_memset.c
@@ -1,14 +1,14 @@
 #include "libft.h"
 
-void	*ft_memset(void *block, int c, size_t size)
+void	*ft_memset(void *block, int c, size_t size) //pointer to memory blk, assigned value in each btye of blk, limit of bytes
 {
 	size_t i;
 
 	i = 0;
 	while (i < size)
 	{
-		((unsigned char *)block)[i] = c;
+		((unsigned char *)block)[i] = c; // assigned i-th byte to c, typecast pointer
 		i++;
 	}
-	return (block);
+	return (block); // return original pointer
 }
