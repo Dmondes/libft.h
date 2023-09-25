6856901a25cfe001fe67efd9b7d0acf81b197c62
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-25 15:59:30 +0800

    Create ft_bzero.c

---
+++ functions/ft_bzero.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+void bzero (void *block, size_t size)
+{
+    size_t i;
+
+	i = 0;
+	while (i < size)
+	{
+		((unsigned char *)block)[i] = '\0'; // set memory btyes to zero
+		i++;
+	}
+}
+
+/*void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, '\0', n);
+}*/
