d1551806a34d9efe8e2fd74f0f8dae3dbe210c39
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-26 16:09:53 +0800

    Create ft_memchr.c

---
+++ functions/ft_memchr.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+void * memchr(const void *block, int c, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (*(unsigned char*)(block + i) == (unsigned char)c) // ensure they are individual btyes
+            return ((void*)(block+ i));
+        i++;
+    }
+    return (NULL);
+}
