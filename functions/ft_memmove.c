95de222fce93641063691d40dfb587c49a5af041
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-27 16:09:42 +0800

    Create ft_memmove.c

---
+++ functions/ft_memmove.c
@@ -0,0 +1,28 @@
+#include "libft.h"
+
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+    size_t i;
+
+    if (!dest || !src)
+        return (NULL); // check for null pointers, in case the function dereferences null pointers
+    i = 0;
+    if (dest > src) // <n, dest comes after src, might have overlap
+    {
+        i = n - 1;
+        while (i >= 0 && i < n) // copies from the end
+        {
+            ((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+            i--;
+        }
+    }
+    else
+    {
+        while (i < n) // no overlap, copies from the start
+        {
+            ((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+            i++;
+        }
+    }
+    return (dest);
+}
