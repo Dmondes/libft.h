dd69d1ca1bce06c124193803f1531d5841760deb
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-27 16:37:40 +0800

    Create ft_memcpy.c

---
+++ functions/ft_memcpy.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+void *ft_memcpy(void *dest, const void *src, size_t n)
+{
+    size_t i;
+
+    if (!dest || !src)
+        return (NULL); // Check for null pointers and return NULL if found
+
+    i = 0; // Initialize the index variable
+
+    while (i < n)
+    {
+        ((unsigned char *)dest)[i] = ((unsigned char *)src)[i]; // Copy one byte at a time
+        i++;
+    }
+
+    return (dest); // Return a pointer to the destination
+}
