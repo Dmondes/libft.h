ad29f378f96900592894cdcc0f4efbb167aaed98
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-28 15:31:19 +0800

    Create ft_strdup.c

---
+++ functions/ft_strdup.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+char	*ft_strdup(char *src)
+{
+	char	*point;
+	int		i;
+	int		len;
+
+	i = 0;
+	len = ft_strlen(src);
+	point = (char *)malloc(sizeof(char) * len); // reserve the space required to store the string
+	if (!point)
+		return (0); // if malloc fails, return 0
+	while (src[i])
+	{
+		point[i] = src[i];
+		i ++;
+	}
+	point[i] = '\0'; // null-terminate the duplicate string
+	return (point); // returns the pointer to the start of the duplicate
+}
+// strdup: returns a dynamically allocated copy of a string, useful to modify duplicate without affecting the original
