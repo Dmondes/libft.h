77ee021b8dbcbabcdbe2061c01e28b8c7acbeb0b
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-10-02 16:06:05 +0800

    Update ft_substr.c

--- functions/ft_substr.c
+++ functions/ft_substr.c
@@ -11,7 +11,7 @@ char *ft_substr(char const *s, unsigned int start, size_t len) {
     s_len = ft_strlen(s);
     if (start >= s_len)
         return (ft_strdup("")); // Return an empty string if start is out of bounds
-    size_t sub_len = (s_len - start < len) ? s_len - start : len;
+    size_t sub_len = (s_len - start < len) ? s_len - start : len; // check if remainder is less than len, extract remainder vs extract all
     char *substr = (char *)malloc((sub_len + 1) * sizeof(char)); // +1 for the null terminator
     if (!substr)
         return (NULL); // Memory allocation failed
