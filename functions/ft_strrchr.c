0854e3bf5a6ec3000642d175154c39427eb67316
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-10-13 15:27:28 +0800

    Update ft_strrchr.c

--- functions/ft_strrchr.c
+++ functions/ft_strrchr.c
@@ -3,13 +3,17 @@
 char  *ft_strrchr(const char *s, int c)
 {
 	int	i;
+	char	ch;
 
 	i = ft_strlen(s); // get the length of string
+	ch = c;
+	if (ch == "\0") // check if ch is the null character
+        	return ((char *)&s[i]); // return a pointer to the null-terminating character
 	while (i >= 0)
 	{
-		if (s[i] == (char)c)
-			return ((char*)(s + i));
-		i--;
+		if (s[i] == ch)
+			return ((char *)&s[i]);
+		i --;
 	}
 	return (NULL);
 }
