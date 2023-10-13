c5e97933790dc58b24625cf5d6afb8540a7138ed
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-10-13 11:13:42 +0800

    Update ft_strchr.c

--- functions/ft_strchr.c
+++ functions/ft_strchr.c
@@ -5,15 +5,15 @@ char	*ft_strchr(const char *s, int c)
 	int		i;
 	char	ch;
 
-	ch = c;
 	i = 0;
+	ch = c; // convert c to ascii character
 	while (s[i])
 	{
 		if (s[i] == ch)
-			return ((char *)s + i);
+			return (&s[i]);
 		i++;
 	}
 	if (ch == '\0') // if the char we are searching for happens to be the null-terminating char
-		return ((char *)s + i);
+		return (&s[i]);//return ((char *)s + i);
 	return (NULL);
 }
