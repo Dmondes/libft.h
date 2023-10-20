d66e7ea03e9a9bf5fdb3c61601938ec79c69cf7f
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-10-20 15:48:17 +0800

    Create ft_strmapi.c

---
+++ functions/ft_strmapi.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+#include <stdlib.h>
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    int len;
+    int i;
+    char *new_str;
+    char ch;
+
+    if (s == NULL || f == NULL)
+        return (NULL);
+    len = ft_strlen(s);
+    new_str = (char *)malloc(sizeof(char) * (len + 1));
+    if (!new_str)
+        return (NULL);
+    i = 0;
+    while (i < len)
+    {
+        ch = *f(i, s[i]);
+        new_str[i] = ch;
+        i ++;
+    }
+    new_str[i] = '\0';
+    return (new_str);
+}
