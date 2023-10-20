4eda9085e176e70bf202bdb6eeec59b3cfaf7097
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-10-20 16:31:57 +0800

    Create ft_putendl_fd.c

---
+++ functions/ft_putendl_fd.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+#include <unistd.h>
+
+void ft_putendl_fd(char *s, int fd)
+{
+    int i = 0;
+
+    while (s[i])
+    {
+        write(fd, &s[i], 1);
+        i++;
+    }
+    write(fd, "\n", 1); 
+}
