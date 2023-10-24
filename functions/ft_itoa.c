512571091352a3c946deaaad7ead0f4d309bfd04
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-10-24 15:54:36 +0800

    Update ft_itoa.c

--- functions/ft_itoa.c
+++ functions/ft_itoa.c
@@ -2,47 +2,52 @@
 #include <stdlib.h>
 
 char *ft_itoa(int n)
-{
+ {
+    int num;
+    int negative;
+    int count;
+    int temp;
+    int i;
+    char *new_str;
+
+    num = n;
+    negative = 0;
+    count = 0;
     if (n == 0)  // Handle the special case of 0
     {
-        char *result = (char *)malloc(2); // malloc 2
-        if (result)
+        new_str = (char *)malloc(2); // malloc 2
+        if (!new_str) 
+        return (NULL);
+        if (new_str)
         {
-            result[0] = '0';
-            result[1] = '\0';
+            new_str[0] = '0';
+            new_str[1] = '\0';
         }
-        return (result);
+        return (new_str);
     }
-    int is_negative = (n < 0) ? 1 : 0; // Determine the sign of the number
-    int num = (is_negative) ? -n : n; // Calculate the number of digits
-    int num_digits = 0;
-    while (num > 0)
+    if (n < 0) // Handle negative numbers
     {
+        num = -n;
+        negative = 1;
+        count++;
+    }
+    temp = num; // Count the number of digits in the number
+    while (temp > 0) 
+    {
+        count++;
+        temp /= 10;
+    }
+    new_str = (char *)malloc(count + 1); // +1 for the null-terminator
+    if (!new_str) 
+        return (NULL);
+    i = count - 1; 
+     while (i >= 0) // Fill the string in reverse order
+     {
+        new_str[i] = num % 10 + '0';
         num /= 10;
-        num_digits++;
+        i --;
     }
-    char *result = (char *)malloc(num_digits + is_negative + 1); // Allocate memory for the result string
-    if (result == NULL)
-        return (NULL);    
-    num = (is_negative) ? -n : n; // Convert the number to a string in reverse order
-    int index = 0;
-    while (num > 0)
-    {
-        result[index++] = (num % 10) + '0';
-        num /= 10;
-    }    
-    if (is_negative) // Add the negative sign if the number is negative
-        result[index++] = '-';
-    result[index] = '\0'; // Add the null-terminator and reverse the string
-    int start = 0;
-    int end = index - 1;
-    while (start < end)
-    {
-        char temp = result[start];
-        result[start] = result[end];
-        result[end] = temp;
-        start++;
-        end--;
-    }
-    return (result);
+    if (negative) 
+        new_str[0] = '-'; // minus sign if negative
+    return (new_str);
 }
