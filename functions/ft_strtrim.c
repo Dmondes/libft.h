83bd7b560af47e73dd30d86516f64573d0e00ce4
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-10-29 14:43:47 +0800

    Update ft_strtrim.c

--- functions/ft_strtrim.c
+++ functions/ft_strtrim.c
@@ -1,33 +1,27 @@
 #include "libft.h"
-#include <stdlib.h>
 
-char *ft_strtrim(char const *s1, char const *set) {
+char *ft_strtrim(char const *s1, char const *set) 
+{
     size_t start;
-    size_t end;
     size_t len;
     size_t i;
     size_t trimmed_len;
     char *trim_str;
 
     start = 0;
-    end = 0;
     i = 0;
-
     if (s1 == NULL || set == NULL) 
         return (NULL);
     len = ft_strlen(s1);
     while (start < len && ft_strchr(set, s1[start]) != NULL) // Find the no of characters to remove from the beginning
-    {
-        start++;
-    }
-    while (end < len && ft_strrchr(set, s1[len - end - 1]) != NULL) // Find the no of characters to remove from the end
-    {
-        end++;
-    }
-     trimmed_len = len - start - end; // len of the trimmed string
+        start ++;
+    while (i < len && ft_strrchr(set, s1[len - i - 1]) != NULL) // Find the no of characters to remove from the end
+        i ++;
+    trimmed_len = len - start - i; // len of the trimmed string
     trim_str = (char *)malloc(sizeof(char) * (trimmed_len + 1));
     if (!trim_str) 
         return (NULL); 
+    i = 0;
     while (i < trimmed_len)
     {
         trim_str[i] = s1[start + i];
