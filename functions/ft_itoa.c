a763e23335b5715ad0cf6652187f78b6e3323947
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-10-31 21:34:11 +0800

    Update ft_itoa.c

--- functions/ft_itoa.c
+++ functions/ft_itoa.c
@@ -1,53 +1,66 @@
 #include "libft.h"
-#include <stdlib.h>
+
+char *case_zero()
+{
+    char *new_str;
+
+    new_str = (char *)malloc(2); // malloc 2
+    if (!new_str) 
+        return (NULL);
+    new_str[0] = '0';
+    new_str[1] = '\0';
+    return (new_str);
+}
+
+int count_digits(int n)
+{
+    int count;
+
+    count = 0;
+    while (n > 0) 
+    {
+        count++;
+        n /= 10;
+    }
+    return (count);
+}
+
+void fill_str(int count, int n, char *new_str, int negative)
+{
+    count -= 1;
+    while (count >= 0) // Fill the string in reverse order
+    {
+        new_str[count] = n % 10 + '0';
+        n /= 10;
+        count --;
+    }
+    if (negative) 
+        new_str[0] = '-'; // minus sign if negative
+}
 
 char *ft_itoa(int n)
  {
-    int num;
     int negative;
     int count;
-    int temp;
-    int i;
     char *new_str;
 
-    num = n;
     negative = 0;
     count = 0;
     if (n == 0)  // Handle the special case of 0
     {
-        new_str = (char *)malloc(2); // malloc 2
-        if (!new_str) 
-        return (NULL);
-        if (new_str)
-        {
-            new_str[0] = '0';
-            new_str[1] = '\0';
-        }
+        new_str = case_zero();
         return (new_str);
     }
     if (n < 0) // Handle negative numbers
     {
-        num = -n;
+        n = -n;
         negative = 1;
         count++;
     }
-    temp = num; // Count the number of digits in the number
-    while (temp > 0) 
-    {
-        count++;
-        temp /= 10;
-    }
+    count += count_digits(n);
     new_str = (char *)malloc(count + 1); // +1 for the null-terminator
     if (!new_str) 
         return (NULL);
-    i = count - 1; 
-     while (i >= 0) // Fill the string in reverse order
-     {
-        new_str[i] = num % 10 + '0';
-        num /= 10;
-        i --;
-    }
-    if (negative) 
-        new_str[0] = '-'; // minus sign if negative
+    fill_str(count, n, new_str, negative);
     return (new_str);
 }
