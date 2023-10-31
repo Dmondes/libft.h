c8dbd912e9aea55706ddcd7b91ee2923a6606bb1
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-10-31 10:14:47 +0800

    Update ft_strtrim.c

--- functions/ft_strtrim.c
+++ functions/ft_strtrim.c
@@ -1,5 +1,17 @@
 #include "libft.h"
 
+static void copy_str(char *trim_str, char const *s1, size_t start, size_t trimmed_len)
+{
+    size_t i;
+
+    i = 0;
+    while (i < trimmed_len)
+    {
+        trim_str[i] = s1[start + i];
+        i ++;
+    }
+    trim_str[i] = '\0';
+}
 char *ft_strtrim(char const *s1, char const *set) 
 {
     size_t start;
@@ -21,12 +33,6 @@ char *ft_strtrim(char const *s1, char const *set)
     trim_str = (char *)malloc(sizeof(char) * (trimmed_len + 1));
     if (!trim_str) 
         return (NULL); 
-    i = 0;
-    while (i < trimmed_len)
-    {
-        trim_str[i] = s1[start + i];
-        i ++;
-    }  
-    trim_str[i] = '\0';
+    copy_str(trim_str, s1, start, trimmed_len); 
     return (trim_str);
 }
