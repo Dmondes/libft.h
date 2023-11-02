64b30bfd0acd8b1744d8445720b9a06712723793
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-11-02 10:16:58 +0800

    Update ft_putnbr_fd.c

--- functions/ft_putnbr_fd.c
+++ functions/ft_putnbr_fd.c
@@ -1,1 +1,23 @@
+#include "libft.h"
+
+void ft_putnbr_fd(int n, int fd)
+{
+    char c;
+    long    num;
+    
+    num = n;
+    if (n == 0) // Base case, stop recursion
+    {
+        ft_putchar_fd('0', fd);
+        return; 
+    }
+    if (n < 0)
+    {
+        ft_putchar_fd('-', fd);
+        num = -num; // Handle INT_MIN
+    }
+    ft_putnbr_fd(num / 10, fd); // Recursive call
+    c = num % 10 + '0'; // Get the last digit and convert it to a character
+    ft_putchar_fd(c, fd); // Print the character
+}
 
