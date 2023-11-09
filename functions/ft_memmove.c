e0e5c7733c4ef7b5cb3b52b9ca0136135b4ba328
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-11-09 17:29:56 +0800

    Update ft_memmove.c

--- functions/ft_memmove.c
+++ functions/ft_memmove.c
@@ -2,7 +2,7 @@
 
 void *ft_memmove(void *dest, const void *src, size_t n)
 {
-    size_t i;
+    int i;
 
     if (!dest || !src)
         return (NULL); // check for null pointers, in case the function dereferences null pointers
