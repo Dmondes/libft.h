ba071a0f2017fa720542424346f224cfa778fe8f
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-11-11 18:39:36 +0800

    Update ft_strlcpy.c

--- functions/ft_strlcpy.c
+++ functions/ft_strlcpy.c
@@ -8,9 +8,7 @@ size_t  ft_strlcpy(char *dest, const char *src, size_t size)
     while (src[i])
     {
         if (i < (size - 1))
-        {
             dest[i] = src[i];
-        }
         if (i == (size - 1))
             dest[i] = '\0';
         i ++;
