a0885664ff9cd2f3e265c095832f3c672f3f4262
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-11-11 17:55:25 +0800

    Update ft_strlcat.c

--- functions/ft_strlcat.c
+++ functions/ft_strlcat.c
@@ -1,21 +1,21 @@
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, const char *src, size_t size)
+size_t    ft_strlcat(char *dest, const char *src, size_t size)
 {
-	size_t	destlen;
-	size_t	srclen;
-	size_t	i;
-    
-   	i = 0;
-	destlen = ft_strlen(dest);
-	srclen = ft_strlen(src);
-	if (size == 0 || size < (destlen + srclen))
-		return (srclen + size);
-	while ((src[i]) && (destlen + i < size - 1))
-	{
-		dest[destlen + i] = src[i]; // append src at the end of dest
-		i ++;
-	}
-	dest[destlen + i] = '\0';
-	return (destlen + srclen);
+    size_t    destlen;
+    size_t    srclen;
+    size_t    i;
+
+    i = 0;
+    destlen = ft_strlen(dest);
+    srclen = ft_strlen(src);
+    if (size == 0 || size <= destlen)
+        return (srclen + size);
+    while ((src[i]) && (destlen + i < size - 1))
+    {
+        dest[destlen + i] = src[i];
+        i ++;
+    }
+    dest[destlen + i] = '\0';
+    return (destlen + srclen);
 }
