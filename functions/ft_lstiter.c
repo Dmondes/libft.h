b9cb99d99905c93fdee5767613a2888990458cb5
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-11-18 11:33:17 +0800

    Create ft_lstiter.c

---
+++ functions/ft_lstiter.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+typedef struct s_list 
+{
+    void *content;
+    struct s_list *next;
+} t_list;
+
+void ft_lstiter(t_list *lst, void (*f)(void *))
+{
+    if (lst == NULL || f == NULL) 
+    {
+        return;
+    }
+
+    while (lst != NULL) 
+    {
+        f(lst->content);
+        lst = lst->next;
+    }
+}
