f6aa2705ec02d9e3f1d483e58edf7a051500a7f8
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-11-18 11:18:52 +0800

    Create ft_lstlast.c

---
+++ functions/ft_lstlast.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+typedef struct s_list 
+{
+    void *content;
+    struct s_list *next;
+} t_list;
+
+t_list *ft_lstlast(t_list *lst) 
+{
+    if (lst == NULL)
+   {
+        return NULL;
+    }
+    while (lst->next != NULL)
+   {
+        lst = lst->next; 
+    }
+    return lst; 
+}
