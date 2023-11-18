7f0f8805e7c4e016794cfde7f42e605ba181a614
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-11-18 15:06:52 +0800

    Update ft_lstnew.c

--- functions/ft_lstnew.c
+++ functions/ft_lstnew.c
@@ -1,11 +1,5 @@
 #include "libft.h"
 
-typedef struct s_list
-{
-    void *content;
-    struct s_list *next;
-} t_list;
-
 t_list *ft_lstnew(void *content)
 {
     t_list *new_node = (t_list *)malloc(sizeof(t_list));
