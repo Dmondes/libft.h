2ce85f6c1a444c776aaf9875d8238ca7cb033fae
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-11-18 11:05:52 +0800

    Update ft_lstadd_front.c

--- functions/ft_lstadd_front.c
+++ functions/ft_lstadd_front.c
@@ -1,1 +1,15 @@
+#include "libft.h"
+typedef struct s_list
+{
+    void *content;
+    struct s_list *next;
+} t_list;
 
+void ft_lstadd_front(t_list **lst, t_list *new)
+{
+    if (lst == NULL || new == NULL)
+        return;
+
+    new->next = *lst;
+    *lst = new;
+}
