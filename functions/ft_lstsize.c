fb5a494e0e343127f91b9ad6e8f90f3bf3ffa422
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-11-18 11:09:32 +0800

    Update ft_lstsize.c

--- functions/ft_lstsize.c
+++ functions/ft_lstsize.c
@@ -1,1 +1,19 @@
+#include "libft.h"
+
+typedef struct s_list 
+{
+    void *content;
+    struct s_list *next;
+} t_list;
+
+int ft_lstsize(t_list *lst)
+ {
+    int count = 0;
+    while (lst != NULL) 
+    {
+        count++; 
+        lst = lst->next; 
+    }
+    return count;
+}
 
