6d737776c0502563d128e32f32672f9e4d08f932
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-11-19 18:01:24 +0800

    Update ft_calloc.c

--- functions/ft_calloc.c
+++ functions/ft_calloc.c
@@ -1,20 +1,13 @@
 #include "libft.h"
 #include <stdlib.h>
 
-void  *ft_calloc(size_t count, size_t size) // number of elements, size in bytes of each element
+void	*ft_calloc(size_t count, size_t size)
 {
-    void    *ptr;
-    
-    if (count == 0 || size == 0)
-        {
-                ptr = (void*)malloc(1);
-                if (ptr)
-                        ft_bzero(ptr, 1);
-                return (ptr);
-        }
-    ptr = (void*)malloc(count * size);
-    if (!ptr)
-        return (NULL);
-    ft_bzero(ptr, count);
-    return (ptr);
+	void	*ptr;
+
+	ptr = (void *)malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, count * size);
+	return (ptr);
 }
