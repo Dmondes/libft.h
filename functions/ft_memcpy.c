60c56c27da04504bd05e5e72c3777049163335c0
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-11-19 18:02:04 +0800

    Update ft_memcpy.c

--- functions/ft_memcpy.c
+++ functions/ft_memcpy.c
@@ -5,7 +5,7 @@ void *ft_memcpy(void *dest, const void *src, size_t n)
     size_t i;
 
     i = 0; // Initialize the index variable
-    if (!dest || !src)
+    if (!dest && !src)
         return (NULL); // Check for null pointers and return NULL if found
     while (i < n)
     {
