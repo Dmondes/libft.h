71a8dec0b34a03d0ffb6977c3c94fa8a808524b5
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-11-20 15:39:17 +0800

    Update ft_substr.c

--- functions/ft_substr.c
+++ functions/ft_substr.c
@@ -8,13 +8,13 @@ char    *ft_substr(char const *s, unsigned int start, size_t len)
 
         i = 0;
         s_len = ft_strlen(s);
-        if (start >= s_len || len == 0)
+        if (start >= s_len)
                 return (ft_strdup(""));
         if (!s)
                 return (NULL);
         if (start + len > s_len)
                 len = s_len - start;
-        new_str = (char *)malloc(sizeof(char) * (len + 1));
+        new_str = (char *)malloc(len + 1);
         if (!new_str)
                 return (NULL);
         while (i < len && s[start + i])
