c9d459ee1c60895353c8544ee67da9a6b3885741
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-11-20 14:22:35 +0800

    Update ft_strtrim.c

--- functions/ft_strtrim.c
+++ functions/ft_strtrim.c
@@ -1,38 +1,50 @@
-#include "libft.h"
-
-static void copy_str(char *trim_str, char const *s1, size_t start, size_t trimmed_len)
+void    copy_str(char *trim_str, char const *s1, int start, int t_len)
 {
-    size_t i;
+        int     i;
 
-    i = 0;
-    while (i < trimmed_len)
-    {
-        trim_str[i] = s1[start + i];
-        i ++;
-    }
-    trim_str[i] = '\0';
+        i = 0;
+        while (i < t_len)
+        {
+                trim_str[i] = s1[start + i];
+                i ++;
+        }
+        trim_str[i] = '\0';
 }
-char *ft_strtrim(char const *s1, char const *set) 
-{
-    size_t start;
-    size_t len;
-    size_t i;
-    size_t trimmed_len;
-    char *trim_str;
 
-    start = 0;
-    i = 0;
-    if (s1 == NULL || set == NULL) 
-        return (NULL);
-    len = ft_strlen(s1);
-    while (start < len && ft_strchr(set, s1[start]) != NULL) // Find the no of characters to remove from the beginning
-        start ++;
-    while (i < len && ft_strrchr(set, s1[len - i - 1]) != NULL) // Find the no of characters to remove from the end
-        i ++;
-    trimmed_len = len - start - i; // len of the trimmed string
-    trim_str = (char *)malloc(sizeof(char) * (trimmed_len + 1));
-    if (!trim_str) 
-        return (NULL); 
-    copy_str(trim_str, s1, start, trimmed_len); 
-    return (trim_str);
+char    *str_zero()
+{
+        char    *trim_str;
+
+        trim_str = (char *)malloc(sizeof(char));
+        if (!trim_str)
+                return (NULL);
+        *trim_str = '\0';
+        return (trim_str);
+}
+
+char    *ft_strtrim(char const *s1, char const *set)
+{
+        int     start;
+        int     len;
+        int     i;
+        int     trimmed_len;
+        char    *trim_str;
+
+        start = 0;
+        i = 0;
+        if (s1 == NULL || set == NULL)
+                return (NULL);
+        len = ft_strlen(s1);
+        while (start < len && ft_strchr(set, s1[start]) != NULL)
+                start ++;
+        while (i < len && ft_strrchr(set, s1[len - i - 1]) != NULL)
+                i ++;
+        trimmed_len = len - start - i;
+        if (trimmed_len <= 0)
+                return (str_zero());
+        trim_str = (char *)malloc(sizeof(char) * (trimmed_len + 1));
+        if (!trim_str)
+                return (NULL);
+        copy_str(trim_str, s1, start, trimmed_len);
+        return (trim_str);
 }
