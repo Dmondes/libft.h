a51f9c9eb65d9499dfefd85e5ed9f1706eda946f
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-11-20 15:37:41 +0800

    Update ft_substr.c

--- functions/ft_substr.c
+++ functions/ft_substr.c
@@ -1,23 +1,27 @@
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char    *ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*new_str;
-	size_t	i;
+        char    *new_str;
+        size_t  i;
+        size_t  s_len;
 
-   	i = 0;
-	if (!s || start >= ft_strlen(s))
+        i = 0;
+        s_len = ft_strlen(s);
+        if (start >= s_len || len == 0)
                 return (ft_strdup(""));
-	if (!s)
-		return (NULL);
-	new_str = (char *)malloc(len + 1); //len + null-terminated
-	if (!new_str)
-		return (NULL);
-	while (i < len && s[start + i]) 
-	{
-		new_str[i] = s[start + i];
-		i++;
-	}
-	new_str[i] = '\0';
-	return (new_str);
+        if (!s)
+                return (NULL);
+        if (start + len > s_len)
+                len = s_len - start;
+        new_str = (char *)malloc(sizeof(char) * (len + 1));
+        if (!new_str)
+                return (NULL);
+        while (i < len && s[start + i])
+        {
+                new_str[i] = s[start + i];
+                i ++;
+        }
+        new_str[i] = '\0';
+        return (new_str);
 }
