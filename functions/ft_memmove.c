ecf5433caf71d00146cbed3eea9c6ed710c440df
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-11-20 10:37:08 +0800

    Update ft_memmove.c

--- functions/ft_memmove.c
+++ functions/ft_memmove.c
@@ -4,7 +4,7 @@ void *ft_memmove(void *dest, const void *src, size_t n)
 {
     size_t i;
 
-    if (!dest || !src)
+    if (!dest && !src)
         return (NULL); // check for null pointers, in case the function dereferences null pointers
     i = 0;
     if (dest > src) // <n, dest comes after src, might have overlap
